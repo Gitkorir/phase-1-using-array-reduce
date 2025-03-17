397854bd2732e856b69d2825b70e5bb05351cb29
Arnold Korir <arnoldkorir201@gmail.com>
2025-03-17 16:33:36 +0300

    use arrat reduce method

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// Array representing the number of batteries in different assemblies
+const assembledBatteries = [
+  5,
+  8,
+  3,
+  2,
+  4,
+  9, // Example battery counts from different assemblies
+];
+
+// Function to calculate the total number of batteries
+const totalBatteries = assembledBatteries.reduce(
+  (sum, batteries) => sum + batteries,
+  0
+);
+
+// Export the variable for testing
+if (typeof module !== "undefined" || module.exports) {
+  module.exports = {
+    totalBatteries,
+  };
+}
